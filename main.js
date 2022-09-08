70611d28cba52730e601b6a3712f0190b7fa4717
hide <hide@hide.com>
2022-09-08 21:58:23 +0800

    [feat]:{exclude all IPs in China.}

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ const { exec } = require('node:child_process')
 
 // pls make sure this is identical from https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/LocationsOfEdgeServers.html.
 const OFFICIAL_AWS_IPs_URL = "https://d7uri8nf7uskq.cloudfront.net/tools/list-cloudfront-ips"
+const PREFIX_IP_LOCALATION = "http://ip2c.org/"
+var ipInfo = require("ip-info-finder");
 
+"use strict"
 
+const Netmask = require('netmask').Netmask
 
 
 function execPromise(command) {
@@ -19,13 +23,49 @@ function execPromise(command) {
     });
 }
 
+let IPs = [];
+let excludeCNIPs = [];
 
+
+// 
 async function main() {
     try {
         var result = await execPromise(`curl ${OFFICIAL_AWS_IPs_URL}`);
         const json = JSON.parse(result);
-        const arrOfIPs = json["CLOUDFRONT_GLOBAL_IP_LIST"];
-        console.log("Output 1: \n", arrOfIPs)
+        // items of this are CIDR, its doc is here https://datatracker.ietf.org/doc/rfc4632/.
+        const arrOfIPRanges = json["CLOUDFRONT_GLOBAL_IP_LIST"];
+
+        for (const ipRnage of arrOfIPRanges) {
+            let ips = new Netmask(ipRnage)
+
+            let ip = ips.first;
+
+            const queryLocationCommand = `curl ${PREFIX_IP_LOCALATION}${ip}`;
+            const resultOfIP = await execPromise(queryLocationCommand);
+            if (resultOfIP.includes(';')) {
+                const nation = resultOfIP.split(";")[1].trim();
+                if (nation !== 'CN') {
+                    excludeCNIPs.push(ip)
+                }
+                else {
+                    console.log(`${ip} is in China.`);
+                    continue;
+                }
+            }
+            else {
+                throw new Error(` unexpected result : ${resultOfIP}.`)
+            }
+
+
+            ips.forEach((item) => {
+                excludeCNIPs.push(item);
+            })
+        }
+
+
+        console.log(`IPs.length is ${excludeCNIPs.length}`);
+
+
     } catch (e) {
         console.error(e.message);
     }
